d36cefc6b8f19819f7724686ecf104c9bad6c15a
Owen Na <newseoul1302@gmail.com>
2022-05-15 04:44:20 -0400

    Initial V0.2
    HyperLink works on the CheckBox Element, need to make sure it doesnt activate the checkbox itself and needs an Actual ToS statement.
    Need to start making main layout and still need to figure how to store info safely into Json.

--- app/src/main/java/com/example/searchprov01/MainActivity.java
+++ app/src/main/java/com/example/searchprov01/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("testing");
         TextView hyperLink = (TextView) findViewById(R.id.checkBox);
 
         hyperLink.setOnClickListener(new View.OnClickListener() {
