f65ea1e1a5e15de7d10615fe31d154c0ace81ed9
Owen Na <newseoul1302@gmail.com>
2022-05-18 05:57:01 -0400

    Initial V0.3
    Made temporary Fix for hyperlinking for ToS, created new Activity and xml for login Page. Need to create safe storage of info ASAP.

--- app/src/main/java/com/example/searchprov01/MainActivity.java
+++ app/src/main/java/com/example/searchprov01/MainActivity.java
@@ -1,24 +1,12 @@
 package com.example.searchprov01;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.basgeekball.awesomevalidation.AwesomeValidation;
-import com.basgeekball.awesomevalidation.ValidationStyle;
-
-import org.w3c.dom.Text;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,17 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView hyperLink = (TextView) findViewById(R.id.checkBox);
-
-        hyperLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Pop.class));
-            }
-        });
+        agreeToToS();
+        toLoginScreen();
     }
 
+        private void agreeToToS () {
+            TextView hyperLink = (TextView) findViewById(R.id.checkBox);
+            hyperLink.setMovementMethod(LinkMovementMethod.getInstance());
+            hyperLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(MainActivity.this, Pop.class));
+                }
+            });
+        }
 
+        private void toLoginScreen() {
+        TextView hyperLink = (TextView) findViewById(R.id.textView2);
+        hyperLink.setMovementMethod(LinkMovementMethod.getInstance());
+            hyperLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(MainActivity.this, Login_page.class));
+                }
+            });
+        }
 }
 
 
