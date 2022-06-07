ac16751a6982913780c5b94c979938e5461ff52b
Owen Na <newseoul1302@gmail.com>
2022-06-07 03:09:56 -0400

    Initial V0.6.4.2
    Created Json methods, need to make sure they work or maybe need to even change them later to write all on one file. Plue need more conditions for proper inputs for inventory values.

--- app/src/main/java/com/example/searchprov01/AddingItem.java
+++ app/src/main/java/com/example/searchprov01/AddingItem.java
@@ -8,11 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class AddingItem extends AppCompatActivity {
 
     Button button;
     Button button2;
-    public int counter = 0;
+    int counter;
+    String priceConverter;
+    String stockConverter;
+    String lengthConverter;
 
     EditText itemNameInput;
     EditText priceInput;
@@ -24,6 +34,8 @@ public class AddingItem extends AppCompatActivity {
     int[] amountInStock = new int[50];
     int[] length = new int[50];
 
+    JSONArray inventory = new JSONArray();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -34,15 +46,14 @@ public class AddingItem extends AppCompatActivity {
         amountInStockInput = findViewById(R.id.editTextNumber);
         lengthInput = findViewById(R.id.editTextTextPersonName2);
 
-        itemName[counter] = itemNameInput.getText().toString().trim();
-        // ?? price[counter] = priceInput.getText().toString().trim();
-        // ?? amountInStock[counter] = amountInStockInput.getText().toString().trim();
-        // ?? length[counter] = lengthInput.getText().toString().trim();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_item);
         toExtra();
-        submitItem();
+        getItemName();
+        getRawPrice();
+        getStockAmount();
+        getLength();
+        exitOut();
     }
 
     private void toExtra() {
@@ -50,23 +61,56 @@ public class AddingItem extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(AddingItem.this, ExtraItemInfo.class));
-            }
-        });
-    }
-
-    private void submitItem() {
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(AddingItem.this, inventoryView.class));
                 makeJsonObject(itemName, price, amountInStock, length);
             }
         });
     }
 
-    public void makeJsonObject(String[] itemName, int[] price, int[] amountInStock, int[] length) {
-
+    private void getItemName() {
+        itemName[counter] = itemNameInput.getText().toString().trim();
     }
-    // NEED JSON METHOD
+
+    private void getRawPrice() {
+        priceConverter = priceInput.getText().toString().trim();
+        price[counter] = Integer.parseInt(priceConverter);
+    }
+
+    private void getStockAmount() {
+        stockConverter = amountInStockInput.getText().toString().trim();
+        amountInStock[counter] = Integer.parseInt(stockConverter);
+    }
+
+    private void getLength() {
+        lengthConverter = lengthInput.getText().toString().trim();
+        length[counter] = Integer.parseInt(lengthConverter);
+    }
+
+    private void exitOut() {
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddingItem.this, inventoryView.class));
+            }
+        });
+    }
+
+    public void makeJsonObject(String[] itemName, int[] price, int[] amountInStock, int[] length) {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("itemName", itemName[counter]);
+            obj.put("price", price[counter]);
+            obj.put("amountInStock", amountInStock[counter]);
+            obj.put("length", length[counter]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        inventory.put(obj);
+        counter++;
+        try (FileWriter file = new FileWriter("inventoryJson.json")) {
+            file.write(inventory.toString());
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+    }
 
 }
--- app/src/main/java/com/example/searchprov01/ExtraItemInfo.java
+++ app/src/main/java/com/example/searchprov01/ExtraItemInfo.java
@@ -6,30 +6,108 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ExtraItemInfo extends AppCompatActivity {
 
     Button button;
+
+    JSONArray secretValues;
+
+    int lastCounter;
+
+    int[] idNumber = new int[50];
+    double[] weight = new double[50];
+    double[] thickness = new double[50];
+    double[] profitRatio = new double[50];
+
+    EditText idNumberInput;
+    EditText weightInput;
+    EditText thicknessInput;
+    EditText profitInput;
+
+    String idNumberConverter;
+    String weightConverter;
+    String thicknessConverter;
+    String profitConverter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         button = findViewById(R.id.button7);
 
+        idNumberInput = findViewById(R.id.editTextNumber4);
+        weightInput = findViewById(R.id.editTextNumberDecimal2);
+        thicknessInput = findViewById(R.id.editTextNumber2);
+        profitInput = findViewById(R.id.editTextNumber3);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extra_item_info);
+
         toAddingItem();
+        getIdNumber();
+        getWeight();
+        getThickness();
+        getProfitRatio();
     }
 
     private void toAddingItem() {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    makeJsonObject(idNumber, weight, thickness, profitRatio);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 startActivity(new Intent(ExtraItemInfo.this, AddingItem.class));
             }
         });
     }
 
-    //  NEED JSON METHOD
+    private void getIdNumber() {
+        idNumberConverter = idNumberInput.getText().toString().trim();
+        idNumber[lastCounter] = Integer.parseInt(idNumberConverter);
+    }
+
+    private void getWeight() {
+        weightConverter = weightInput.getText().toString().trim();
+        weight[lastCounter] = Integer.parseInt(weightConverter);
+    }
+
+    private void getThickness() {
+        thicknessConverter = thicknessInput.getText().toString().trim();
+        thickness[lastCounter] = Integer.parseInt(thicknessConverter);
+    }
+
+    private void getProfitRatio() {
+        profitConverter = profitInput.getText().toString().trim();
+        profitRatio[lastCounter] = Integer.parseInt(profitConverter);
+    }
+
+    private void makeJsonObject(int[] idNumber, double[] weight, double[] thickness, double[] profitRatio) {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("IDNumber", idNumber[lastCounter]);
+            obj.put("Weight", weight[lastCounter]);
+            obj.put("Thickness", thickness[lastCounter]);
+            obj.put("ProfitRatio", profitRatio[lastCounter]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        secretValues.put(obj);
+        try (FileWriter file = new FileWriter("SecretValues.json")) {
+            file.write(secretValues.toString());
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+    }
 
 }
