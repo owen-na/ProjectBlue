6f44bfe19f55dc159d6ba0af77241756c28d938c
Darren Tham <darrent9859@gmail.com>
2022-06-19 14:39:28 -0400

    Added new class for organization

---
+++ app/src/main/java/com/example/searchprov01/InputChecker.java
@@ -0,0 +1,47 @@
+package com.example.searchprov01;
+
+import android.widget.EditText;
+
+public class InputChecker extends AppCompatActivity {
+    protected String stringify(EditText editText) {
+        return editText.getText().toString().trim();
+    }
+
+    protected void createToast(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT);
+    }
+
+    protected boolean stringValid(String str, String blankError) {
+        boolean isEmpty = str.isEmpty();
+        if (isEmpty) {
+            createToast(blankError);
+        }
+        return isEmpty;
+    }
+
+    protected boolean intValid(String str, String numError, String nanError) {
+        try {
+            int i = Integer.parseInt(str);
+            if (i <= 0) {
+                createToast(numError);
+            }
+            return i > 0;
+        } catch (NumberFormatException e) {
+            createToast(nanError);
+            return false;
+        }
+    }
+
+    protected boolean doubleValid(String str, String numError, String nanError) {
+        try {
+            double d = Double.parseDouble(str);
+            if (d <= 0.0) {
+                createToast(numError);                
+            }
+            return d > 0.0;
+        } catch (NumberFormatException e) {
+            createToast(nanError);
+            return false;
+        }
+    }
+}
