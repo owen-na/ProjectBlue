fb0cb68efd4bfa71224d6f5ef8ef195fec67d75c
Owen Na <newseoul1302@gmail.com>
2022-06-26 22:00:38 -0400

    Initial V0.7.1
    *WIP*, Need to make sure items are added properly to the FireBaseDataBase

--- app/src/main/java/com/example/searchprov01/AddingItem.java
+++ app/src/main/java/com/example/searchprov01/AddingItem.java
@@ -7,13 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AddingItem extends AppCompatActivity{
 
